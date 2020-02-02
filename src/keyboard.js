32ff83bf7088429558bd22e6b1ce285bbb9e6b3b
harphield <harphield@gmail.com>
2020-02-02 15:06:44 +0100

    Tiles customization

--- src/keyboard.js
+++ src/keyboard.js
@@ -4,11 +4,51 @@ export default class MahjongKeyboard {
     constructor({
         keyboard = '.mj-keyboard',
         opener = '.mj-kb-open',
-        placeAfter = '.mj-keyboard'
+        placeAfter = '.mj-keyboard',
+        tiles = {
+            '1m': '&#126983;',
+            '2m': '&#126984;',
+            '3m': '&#126985;',
+            '4m': '&#126986;',
+            '5m': '&#126987;',
+            '6m': '&#126988;',
+            '7m': '&#126989;',
+            '8m': '&#126990;',
+            '9m': '&#126991;',
+
+            '1p': '&#127001;',
+            '2p': '&#127002;',
+            '3p': '&#127003;',
+            '4p': '&#127004;',
+            '5p': '&#127005;',
+            '6p': '&#127006;',
+            '7p': '&#127007;',
+            '8p': '&#127008;',
+            '9p': '&#127009;',
+
+            '1s': '&#126992;',
+            '2s': '&#126993;',
+            '3s': '&#126994;',
+            '4s': '&#126995;',
+            '5s': '&#126996;',
+            '6s': '&#126997;',
+            '7s': '&#126998;',
+            '8s': '&#126999;',
+            '9s': '&#127000;',
+
+            '1z': '&#126976;',
+            '2z': '&#126977;',
+            '3z': '&#126978;',
+            '4z': '&#126979;',
+            '5z': '&#126982;',
+            '6z': '&#126981;',
+            '7z': '&#126980;',
+        }
     }) {
         this.keyboard = keyboard;
         this.opener = opener;
         this.placeAfter = placeAfter;
+        this.tiles = tiles;
 
         this.suits = [
             'm', 'p', 's', 'z'
@@ -47,46 +87,46 @@ export default class MahjongKeyboard {
         <table class="mj-kb-table">
         <tbody>
             <tr>
-                <td><a class="mj-kb-tile" data-tile="1m">&#126983;</a></td>
-                <td><a class="mj-kb-tile" data-tile="2m">&#126984;</a></td>
-                <td><a class="mj-kb-tile" data-tile="3m">&#126985;</a></td>
-                <td><a class="mj-kb-tile" data-tile="4m">&#126986;</a></td>
-                <td><a class="mj-kb-tile" data-tile="5m">&#126987;</a></td>
-                <td><a class="mj-kb-tile" data-tile="6m">&#126988;</a></td>
-                <td><a class="mj-kb-tile" data-tile="7m">&#126989;</a></td>
-                <td><a class="mj-kb-tile" data-tile="8m">&#126990;</a></td>
-                <td><a class="mj-kb-tile" data-tile="9m">&#126991;</a></td>
+                <td><a class="mj-kb-tile" data-tile="1m">${this.tiles['1m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="2m">${this.tiles['2m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="3m">${this.tiles['3m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="4m">${this.tiles['4m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="5m">${this.tiles['5m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="6m">${this.tiles['6m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="7m">${this.tiles['7m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="8m">${this.tiles['8m']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="9m">${this.tiles['9m']}</a></td>
             </tr>
             <tr>
-                <td><a class="mj-kb-tile" data-tile="1p">&#127001;</a></td>
-                <td><a class="mj-kb-tile" data-tile="2p">&#127002;</a></td>
-                <td><a class="mj-kb-tile" data-tile="3p">&#127003;</a></td>
-                <td><a class="mj-kb-tile" data-tile="4p">&#127004;</a></td>
-                <td><a class="mj-kb-tile" data-tile="5p">&#127005;</a></td>
-                <td><a class="mj-kb-tile" data-tile="6p">&#127006;</a></td>
-                <td><a class="mj-kb-tile" data-tile="7p">&#127007;</a></td>
-                <td><a class="mj-kb-tile" data-tile="8p">&#127008;</a></td>
-                <td><a class="mj-kb-tile" data-tile="9p">&#127009;</a></td>
+                <td><a class="mj-kb-tile" data-tile="1p">${this.tiles['1p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="2p">${this.tiles['2p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="3p">${this.tiles['3p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="4p">${this.tiles['4p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="5p">${this.tiles['5p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="6p">${this.tiles['6p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="7p">${this.tiles['7p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="8p">${this.tiles['8p']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="9p">${this.tiles['9p']}</a></td>
             </tr>
             <tr>
-                <td><a class="mj-kb-tile" data-tile="1s">&#126992;</a></td>
-                <td><a class="mj-kb-tile" data-tile="2s">&#126993;</a></td>
-                <td><a class="mj-kb-tile" data-tile="3s">&#126994;</a></td>
-                <td><a class="mj-kb-tile" data-tile="4s">&#126995;</a></td>
-                <td><a class="mj-kb-tile" data-tile="5s">&#126996;</a></td>
-                <td><a class="mj-kb-tile" data-tile="6s">&#126997;</a></td>
-                <td><a class="mj-kb-tile" data-tile="7s">&#126998;</a></td>
-                <td><a class="mj-kb-tile" data-tile="8s">&#126999;</a></td>
-                <td><a class="mj-kb-tile" data-tile="9s">&#127000;</a></td>
+                <td><a class="mj-kb-tile" data-tile="1s">${this.tiles['1s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="2s">${this.tiles['2s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="3s">${this.tiles['3s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="4s">${this.tiles['4s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="5s">${this.tiles['5s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="6s">${this.tiles['6s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="7s">${this.tiles['7s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="8s">${this.tiles['8s']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="9s">${this.tiles['9s']}</a></td>
             </tr>
             <tr>
-                <td><a class="mj-kb-tile" data-tile="1z">&#126976;</a></td>
-                <td><a class="mj-kb-tile" data-tile="2z">&#126977;</a></td>
-                <td><a class="mj-kb-tile" data-tile="3z">&#126978;</a></td>
-                <td><a class="mj-kb-tile" data-tile="4z">&#126979;</a></td>
-                <td><a class="mj-kb-tile" data-tile="5z">&#126982;</a></td>
-                <td><a class="mj-kb-tile" data-tile="6z">&#126981;</a></td>
-                <td><a class="mj-kb-tile" data-tile="7z">&#126980;</a></td>
+                <td><a class="mj-kb-tile" data-tile="1z">${this.tiles['1z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="2z">${this.tiles['2z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="3z">${this.tiles['3z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="4z">${this.tiles['4z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="5z">${this.tiles['5z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="6z">${this.tiles['6z']}</a></td>
+                <td><a class="mj-kb-tile" data-tile="7z">${this.tiles['7z']}</a></td>
                 <td class="mj-kb-undo"><a>&#8630;</a></td>
                 <td class="mj-kb-clear"><a>&#128473;</a></td>
             </tr>
@@ -111,15 +151,15 @@ export default class MahjongKeyboard {
             return false;
         });
 
-        const tiles = layout.querySelectorAll('.mj-kb-tile');
+        const tiles = layout.querySelectorAll('a.mj-kb-tile');
         tiles.forEach((tile) => {
             tile.addEventListener('click', (e) => {
                 let ho = this.splitHand(kb.value);
 
                 if (this.countHandObject(ho) === 13) {
-                    kb.value = this.joinHand(ho) + e.target.getAttribute('data-tile');
+                    kb.value = this.joinHand(ho) + tile.getAttribute('data-tile');
                 } else {
-                    ho = this.addToHandObject(ho, e.target.getAttribute('data-tile'));
+                    ho = this.addToHandObject(ho, tile.getAttribute('data-tile'));
                     kb.value = this.joinHand(ho);
                 }
 
